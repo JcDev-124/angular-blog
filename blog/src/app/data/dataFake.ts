5124d10c2d9dc0d556974eaa3eae81c24e4d770c
julio <julio.prog124@gmail.com>
2023-10-22 20:24:36 -0300

    dinamismo de rotas

---
+++ blog/src/app/data/dataFake.ts
@@ -0,0 +1,11 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "photo": "aaa",
+        "title": "Saiu um novo filme",
+        "description": "ola",
+        "photoCover": ""
+
+    }
+
+] 
